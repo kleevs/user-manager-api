02ce561b5aaade624ae2fa3e441bcc55818388d3
Kevin Landa <kev.sam.landa@gmail.com>
2021-04-04 13:00:51 +0200

    clean

--- src/UserManager.Implementation/Exception/FieldRequiredException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserManager.Implementation.Exception
-{
-    public class FieldRequiredException : BusinessException
-    {
-        public FieldRequiredException(int code, string name) : base(code, $"{name} est requis") { }
-    }
-}
--- src/UserManager.Implementation/Exception/LoginException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserManager.Implementation.Exception
-{
-    public class LoginException : BusinessException
-    {
-        public LoginException() : base(Constant.CodeError.LoginFailed, "Login failed") { }
-    }
-}
